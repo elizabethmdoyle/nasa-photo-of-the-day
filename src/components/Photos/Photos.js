186588ddfa1d64ae2212cdc7beacf38f7eb4a365
Elizabeth Doyle <myoddjourney@gmail.com>
2023-06-02 10:47:52 -0400

    minor css work

--- src/components/Photos/Photos.js
+++ src/components/Photos/Photos.js
@@ -5,12 +5,12 @@ const Photos = (props) => {
 
     return (
         <div className="nasa-photo-wrapper">
+            <h1>NASA Photo of the Day</h1>
             <img src={props.photo.hdurl} alt={props.photo.title}/>
-            <h3>{props.photo.title}</h3>
+            <h1>{props.photo.title}</h1>
             <p>{props.photo.date}</p>
             <p>{props.photo.explanation}</p>
             <p>{props.photo.copyright}</p>
-            <button>Photo Details</button>
         </div>
     )
 }
